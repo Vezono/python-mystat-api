1fcce9cef07f1fab101a4ed8f61af6b9c756a4a6
vezono <vezonolab@gmail.com>
2020-09-06 07:50:00 +0300

    refactoring

--- wrapper/api.py
+++ wrapper/api.py
@@ -6,17 +6,8 @@ class Api:
         self.api_url = 'https://msapi.itstep.org/api/v2'
         self.application_key = '6a56a5df2667e65aab73ce76d1dd737f7d1faef9c52e8b8c55ac75f565d8e8a6'
 
-        self.user_credentials = dict()
 
-    def api_get_request(self, method):
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
-
-    @property
-    def access_token(self):
-        return self.user_credentials['access_token']
-
+class PublicApi(Api):
     @property
     def all_cities(self):
         method = '/public/cities'
@@ -32,13 +23,29 @@ class Api:
     def translation(self, lang=None):
         """
         :param lang: you can get langs from all_languages
-        :return:
         """
         method = '/public/translations'
         response = requests.get(f'{self.api_url}{method}', params={'language': lang})
         return response.json()
 
-    def login(self, username, password) -> requests.request:
+
+class StudentApi(Api):
+    def __init__(self, username, password):
+        super().__init__()
+        self.user_credentials = self.login(username, password).json()
+        self.access_token = self.user_credentials['access_token']
+        self.auth_headers = {'Authorization': f'Bearer {self.access_token}'}
+
+    def api_request(self, method, url_method='GET', data=None, params=None):
+        response = requests.request(url=f'{self.api_url}{method}',
+                                    method=url_method,
+                                    headers=self.auth_headers,
+                                    data=data,
+                                    params=params
+                                    )
+        return response
+
+    def login(self, username, password):
         method = '/auth/login'
         response = requests.post(f'{self.api_url}{method}',
                                  data={'username': username,
@@ -51,261 +58,218 @@ class Api:
     @property
     def student_achievements(self):
         method = '/profile/statistic/student-achievements'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     def materials_list(self, material_type):
-        method = '/library/operations/list'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'material_type': material_type})
-        return response.json()
+        method = '/profile/statistic/student-achievements'
+        return self.api_request(method, params={'material_type': material_type}).json()
 
     @property
     def leader_group_points(self):
         method = '/dashboard/progress/leader-group-points'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def leader_group(self):
         method = '/dashboard/progress/leader-group'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def leader_stream_points(self):
         method = '/dashboard/progress/leader-stream-points'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def leader_stream(self):
         method = '/dashboard/progress/leader-stream'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def study_activity_log(self):
         method = '/dashboard/progress/activity'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def counter_homework(self):
         method = '/count/homework'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def average_progress_chart(self):
         method = '/dashboard/chart/average-progress'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def attendance_chart(self):
         method = '/dashboard/chart/attendance'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def future_exams(self):
         method = '/dashboard/info/future-exams'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def user_info(self):
         method = '/settings/user-info'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def evaluation_list(self):
         method = '/feedback/students/evaluate-lesson-list'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def city_contacts(self):
         method = '/contacts/operations/index'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def student_visits(self):
         method = '/progress/operations/student-visits'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def exams(self):
         method = '/progress/operations/student-exams'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def study_plan_url(self):
         method = '/progress/operations/plan-url'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def progress_chart(self):
         method = '/dashboard/chart/progress'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def history_specs(self):
         method = '/settings/history-specs'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def group_specs(self):
         method = '/settings/group-specs'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def group_history(self):
         method = '/homework/settings/group-history'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def homework_list(self):
         method = '/homework/operations/list'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     def schedule_month(self, date_filter=None):
         method = '/schedule/operations/get-month'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'date_filter': date_filter})
-        return response.json()
+        return self.api_request(method, params={'date_filter': date_filter}).json()
 
     def schedule_date(self, date_filter=None):
         method = '/schedule/operations/get-by-date'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'date_filter': date_filter})
-        return response.json()
+        return self.api_request(method, params={'date_filter': date_filter}).json()
 
     def month_events(self, date_filter=None):
         method = '/schedule/operations/month-events'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'date_filter': date_filter})
-        return response.json()
+        return self.api_request(method, params={'date_filter': date_filter}).json()
 
     def products_list(self, page=1, product_type=0):
         method = '/market/customer/product/list'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'page': page, 'type': product_type})
-        return response.json()
+        return self.api_request(method, params={'page': page, 'type': product_type}).json()
 
-    def pushchased_list(self, page=1, product_type=0):
+    def pushchased_list(self, page=1):
         method = '/market/customer/order/list'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'page': page, 'type': product_type})
-        return response.json()
+        return self.api_request(method, params={'page': page}).json()
 
     def pushchased_info(self, product_id=0):
         method = '/market/customer/order/info'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'id': product_id})
-        return response.json()
+        return self.api_request(method, params={'id': product_id}).json()
 
     @property
     def signal_problems_list(self):
         method = '/signal/operations/problems-list'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     @property
     def signals_list(self):
         method = '/signal/operations/signals-list'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     def signals_comments(self, signal_id=0):
         method = '/signal/operations/signals-comments'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'id': signal_id})
-        return response.json()
+        return self.api_request(method, params={'id': signal_id}).json()
 
     @property
     def unread_signals(self):
         method = '/signal/operations/count-unread'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
 
     def signal_reference_data(self, reference_type=0):
         method = '/signal/operations/get-reference-data'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'type': reference_type})
-        return response.json()
+        return self.api_request(method, params={'type': reference_type}).json()
 
     @property
     def signal_reference_status(self):
         method = '/signal/operations/get-reference-status'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.text
+        return self.api_request(method).text
 
     @property
     def student_itstep_feedbacks(self):
         method = '/feedback/social-review/get-review-list'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     @property
     def itstep_reviewing_instruction(self):
         method = '/reviews/index/instruction'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     @property
     def profile_data(self):
         method = '/profile/operations/settings'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     def send_sms(self):
         method = '/contacts/sms/send-code'
-        response = requests.post(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method, url_method='POST').json()
 
     @property
     def recomendations_list(self):
         method = '/referral/operations/list'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     @property
     def payment_info(self):
         method = '/payment/operations/index'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     @property
     def payment_plan(self):
         method = '/payment/operations/schedule'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     @property
     def payment_history(self):
         method = '/payment/operations/history'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.json()
+        return self.api_request(method).json()
 
     @property
     def opened_interview(self):
         method = '/library/quiz/opened-interview'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.text
+        return self.api_request(method).text
 
     @property
     def latest_news(self):
         method = '/news/operations/latest-news'
-        response = requests.get(f'{self.api_url}{method}', headers={'Authorization': f'Bearer {self.access_token}'})
-        return response.text
+        return self.api_request(method).text
 
     def news_info(self, news_id=0):
         method = '/news/operations/detail-news'
-        response = requests.get(f'{self.api_url}{method}',
-                                headers={'Authorization': f'Bearer {self.access_token}'},
-                                params={'news_id': news_id})
-        return response.json()
+        return self.api_request(method, params={'news_id': news_id}).json()
 
     @property
     def unread_news(self):
         method = '/news/operations/count-unread'
-        return self.api_get_request(method)
+        return self.api_request(method).json()
